5c6a0bd89567e07290d6933a0f9f0485257c38d0
andressa <andressacolinbarbosa@gmail.com>
2024-11-18 17:48:40 -0400

    leetcode

--- leetcode1652.py
+++ leetcode1652.py
@@ -1,17 +1,19 @@
-code = list(map(int,input().split()))
-k = int(input())
-n = len(code)
-circular_code = code * 2
-decrypted_code = []
+class Solution(object):
+    def decrypt(self, code, k):
+        n = len(code)
+        circular_code = code * 2
+        decrypted_code = []
 
-for i in range(n):
-    if(k > 0):
-        sum_elements = sum(circular_code[ i + 1 : i + 1 + k])
-        decrypted_code.append(sum_elements)
-    elif(k < 0):
-        sum_elements = sum(circular_code[ i + n + k : i + n])
-        decrypted_code.append(sum_elements)
-    elif(k == 0):
-        decrypted_code.append(0)
+        for i in range(n):
+            if k > 0:
+                sum_elements = sum(circular_code[i + 1 : i + 1 + k])
+                decrypted_code.append(sum_elements)
+            elif k < 0:
+                sum_elements = sum(circular_code[i + n + k : i + n])
+                decrypted_code.append(sum_elements)
+            elif k == 0:
+                decrypted_code.append(0)
+        return decrypted_code
 
-print(decrypted_code)
+
+        
