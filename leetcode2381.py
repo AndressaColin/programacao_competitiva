19b8d46b375e0c543c0c6f2bceae0bb5ef65267e
andressa <andressacolinbarbosa@gmail.com>
2025-01-21 08:30:04 -0400

    #

--- leetcode2381.py
+++ leetcode2381.py
@@ -1,2 +1,9 @@
-s = input().split()
-print(s)
+class Solution(object):
+    def shiftingLetters(self, s, shifts):
+        n = len(s)
+        diffarray = [0]*n
+        for shift in shifts:
+            if shift[2] == 1:
+                diffarray[shift[0]] += 1
+                if shift[1] + 1 < n:
+                    
