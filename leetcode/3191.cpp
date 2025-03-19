0ce98f1e9f3920533e95e1a579df037ef491f3ef
AndressaColin <andressacolinbarbosa@gmail.com>
2025-03-19 12:25:55 -0400

    #

--- leetcode/3191.cpp
+++ leetcode/3191.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <deque>
 using namespace std;
+
+class Solution {
+    public:
+        int minOperations(vector<int>& nums) {
+            int tam = nums.size();
+            int cont = 0;
+            for(int i=0; i<tam-2; i++){ // Verifique até o antepenúltimo elemento
+                if(nums[i]==0){
+                    nums[i] = 1 - nums[i];
+                    nums[i+1] = 1 - nums[i+1];
+                    nums[i+2] = 1 - nums[i+2];
+                    cont++;
+                }
+            }
+        }
+    };
 
 int main()
 {
-    printf("hello world");
-    
+    vector<int> nums = {0,1,1,1};
+    int result;
+    Solution sol;
+    result = sol.minOperations(nums);
     return 0;
 }
